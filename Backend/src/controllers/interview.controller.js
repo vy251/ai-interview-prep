f587eeffb08d37ed82adf8a52f68eb9b2057408b
Vishal Yadav <vy1370382@gmail.com>
2026-08-15 01:11:39 +0530

    Fix import path casing for interview report model in interview controller

--- Backend/src/controllers/interview.controller.js
+++ Backend/src/controllers/interview.controller.js
@@ -1,6 +1,6 @@
 const pdfParse = require("pdf-parse")
 const { generateInterviewReport, generateResumePdf } = require("../services/ai.service")
-const interviewReportModel = require("../models/interviewReport.model")
+const interviewReportModel = require("../models/InterviewReport.model")
 
 
 
