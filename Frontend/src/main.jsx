00511cd6f4fad41e8449bea80584822676fafa7d
Vishal Yadav <vy1370382@gmail.com>
2026-08-15 18:14:00 +0530

    Fix import path casing for ErrorBoundary component in main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import ErrorBoundary from './ErrorBoundary.jsx'
+import ErrorBoundary from './Errorboundary.jsx'
 import "./style.scss";
 
 createRoot(document.getElementById('root')).render(
